792562109ee2797af356de53e0d809e5b82d6453
Pawel <pawelkordel@gmail.com>
2021-12-05 10:04:08 +0100

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Nazywam sie Pawel");
+        System.out.println("Pierwszy raz pracuje z Java");
+        System.out.println("Mam" + " " + "nadzieje " + ",że" + " " + "mnie nie przerośnie!");
+
+        System.out.printf("Teraz %s jak działa %d sposób.\n", "sprawdzam", 2);
+
+        System.out.print("Jestem");
+        System.out.print(" gotowy");
+        System.out.print(" do nauki.");
+
     }
 }

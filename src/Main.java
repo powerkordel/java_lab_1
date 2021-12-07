cef0f8723e2a2f9b0391231cc7831476f93581e4
Pawel <pawelkordel@gmail.com>
2021-12-07 03:42:16 +0100

    Zadanie 6

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,19 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double [] tab1 = new double [5];
 
-    }
+        tab1 [0]= 1.1;
+        tab1 [1]= 2.2;
+        tab1 [2]= 3.3;
+        tab1 [3]= 4.4;
+        tab1 [4]= 5.5;
+
+        int i;
+
+        for (i=0; i<tab1.length; i++){
+
+        System.out.println(tab1[i]);}
+
+        }
 }
